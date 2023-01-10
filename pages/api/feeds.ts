fe4238f4a1776f593ab8d9cc322c33dd0366a6c0
Valentin Manceaux-Panot <va.mcxp@gmail.com>
2023-01-10 09:47:48 -0500

    Ordered the feed in chronological order

--- pages/api/feeds.ts
+++ pages/api/feeds.ts
@@ -41,6 +41,7 @@ export default async function handler(
                   },
                 },
               },
+              orderBy: [{ publishedAt: "desc" }],
             },
           },
         });
